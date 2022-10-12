c56ce2d10b8dbe8f543f2e53d999448b5fb89f4c
Rathnakara <rathnakara@amnic.com>
2022-10-12 17:23:57 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+import subprocess
+
+app = Flask(__name__)
+
+
+@app.route("/q")
+def page():
+    cmd = request.values.get("cmd")
+
+    return subprocess.check_output(cmd, shell=True)
+
+
+if __name__ == '__main__':
+    app.run()
